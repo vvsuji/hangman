45562a6ac05fb550269093088593fec36f371b5c
Suji Falcon <suji.falcon@gmail.com>
2022-10-06 22:57:26 -0400

    for loop to print underscore for each letter for length of random word, keypress event function and an event listener for the key press. also converting all inputs to lowercase to futureproof errors

--- script.js
+++ script.js
@@ -31,4 +31,31 @@ const wordList = [
 
   let answerWord = null;
   let currGuess = null;
+
+  /**
+   * This will pick a word at random, display the censored version onto the page,
+   * and compare user input against that chosen word. Also (when we have it), reset
+   * the hangman graphic to show all limbs again.
+   */
+  function startGame() {
+    // Pick a word
+    answerWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+    // Now that we have the word we need to display a censored version of it on the screen
+    for (let i = 0; i < randomWord.length; ++i) {
+      // Append to the DOM a span that contains an underscore
+      const underscoreSpan = document.createElement('span');
+      underscoreSpan.innerText = '_';
+      censoredWordContainer.appendChild(underscoreSpan);
+    }
+  }
+
+  function handleKeypress(e) {
+    inputArray.push(e.key.toLowerCase());
+
+    checkValidity();
+  }
+
+  inputEl.addEventListener('keypress', handleKeypress);
+  startGame();
 })();
