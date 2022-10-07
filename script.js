0a6bcdf1f90a94eb2b6618d2624a7d98e04b94a8
Suji Falcon <suji.falcon@gmail.com>
2022-10-07 16:02:41 -0400

    added clickability to on screen keyboard, incorrect/correct classes for guesses, and converting und3rscore to correct letter on screen

--- script.js
+++ script.js
@@ -27,10 +27,11 @@ const wordList = [
   const censoredWordContainer = document.querySelector(
     '.censoredWordContainer',
   );
-  const inputEl = document.querySelector('input');
+  const lettersArr = document.querySelectorAll('.letter');
+  // const inputEl = document.querySelector('input');
 
-  let answerWord = null;
-  let currGuess = null;
+  let answerWord = '';
+  let incompArr = [];
 
   /**
    * This will pick a word at random, display the censored version onto the page,
@@ -40,22 +41,61 @@ const wordList = [
   function startGame() {
     // Pick a word
     answerWord = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(answerWord);
 
     // Now that we have the word we need to display a censored version of it on the screen
-    for (let i = 0; i < randomWord.length; ++i) {
+    for (let i = 0; i < answerWord.length; ++i) {
       // Append to the DOM a span that contains an underscore
       const underscoreSpan = document.createElement('span');
+      incompArr.push('_');
       underscoreSpan.innerText = '_';
       censoredWordContainer.appendChild(underscoreSpan);
     }
+
+    lettersArr.forEach((letter) => {
+      letter.addEventListener('click', handleLetterClick);
+    });
+
+    // Set input maxLength
+    // inputEl.maxLength = answerWord.length;
+    // Add eventListener
   }
 
-  function handleKeypress(e) {
-    inputArray.push(e.key.toLowerCase());
+  /**
+   * Check if value entered matches hidden word selected (or whatever the fuck)
+   *
+   * Check if answerWord & key have any letters in common. If so, which?
+   */
+  function checkValidity(target, key) {
+    // We have two strings, answerWord and key.
+    // We need to determine
+    if (answerWord.includes(key)) {
+      // You win.. TODO: Figure out what happens when you win
+      target.classList.add('correct');
 
-    checkValidity();
+      // Find index of letter and replace dash in array with letter
+      let index = answerWord.indexOf(key);
+      incompArr.splice(index, 1, key);
+
+      // Print updated word list
+      censoredWordContainer.innerHTML = '';
+      incompArr.forEach((letter) => {
+        const underscoreSpan = document.createElement('span');
+        underscoreSpan.innerText = letter;
+        censoredWordContainer.appendChild(underscoreSpan);
+      });
+    } else {
+      target.classList.add('incorrect');
+    }
   }
 
-  inputEl.addEventListener('keypress', handleKeypress);
+  function handleLetterClick(e) {
+    let target = e.target;
+    let key = e.target.dataset.letter;
+
+    checkValidity(target, key);
+  }
+
+  // inputEl.addEventListener('keypress', handleClick);
   startGame();
 })();
