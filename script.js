b3d7b09230922a647cfdbdc1b020e41b935fb00b
Suji Falcon <suji.falcon@gmail.com>
2022-10-09 17:58:41 -0400

    added lives, change color on win/lose, and reset button. working on svg still

--- script.js
+++ script.js
@@ -5,97 +5,186 @@
  * - You also don't have to worry about *third party* scripts messing with your scripts
  */
 
-const wordList = [
-  'word',
-  'hello',
-  'world',
-  'slay',
-  'gossip',
-  'girl',
-  'twiliGht',
-  'what',
-  'we',
-  'do',
-  'in',
-  'the',
-  'shadows',
-  'bloodborne',
-  'vex',
-];
+// Constant for number of lives user starts each game with.
+const INITIAL_LIVES = 6;
 
 (() => {
+  // DEFINE GLOBAL VARIABLES
+
+  // List of valid words that we choose from to determine the answerWord.
+  // Using a set so we can easily remove a word from the possibility list each time its used
+  const wordList = new Set([
+    'word',
+    'hello',
+    'world',
+    'slay',
+    'gossip',
+    'girl',
+    'twiliGht',
+    'what',
+    'we',
+    'do',
+    'in',
+    'the',
+    'shadows',
+    'bloodborne',
+    'vex',
+  ]);
   const censoredWordContainer = document.querySelector(
     '.censoredWordContainer',
   );
-  const lettersArr = document.querySelectorAll('.letter');
-  // const inputEl = document.querySelector('input');
+  const lettersDivArr = document.querySelectorAll('.letter');
+  const restartBtn = document.querySelector('#restartBtn');
+  const livesDisplay = document.querySelector('#livesDisplay');
+  const statusBox = document.querySelector('.statusBox');
 
+  // The chosen word for this game, only set initially in startGame & not modified.
   let answerWord = '';
-  const incompArr = [];
+
+  // An array of characters that is used to keep track the current guess;
+  // initialized with underscores and filled in with the correct characters
+  // as they're guessed. (for example, if answerWord = 'hello', currentGuess = ['_', '_', '_'. '_'. '_'])
+  let currentGuess = [];
+
+  // Lives is used to determine which parts of fiddlesticks to show depending on how
+  // many lives (guesses) you have left before you lose the game.
+  // You can pick how many lives you want the player to have, its initialized here:
+  let currLives = INITIAL_LIVES;
+
+  // END GLOBAL VARIABLES
 
   /**
+   * Function called to start (or restart) the game.
    * This will pick a word at random, display the censored version onto the page,
    * and compare user input against that chosen word. Also (when we have it), reset
    * the hangman graphic to show all limbs again.
    */
   function startGame() {
+    // Reset lives
+    updateLives(INITIAL_LIVES);
+
     // Pick a word
-    answerWord = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(answerWord);
+    answerWord = Array.from(wordList.values())[
+      Math.floor(Math.random() * wordList.size)
+    ];
+    console.log(`DEV CHEAT: answerWord is ${answerWord}`);
 
-    // Now that we have the word we need to display a censored version of it on the screen
-    for (let i = 0; i < answerWord.length; ++i) {
-      // Append to the DOM a span that contains an underscore
-      const underscoreSpan = document.createElement('span');
-      incompArr.push('_');
-      underscoreSpan.innerText = '_';
-      censoredWordContainer.appendChild(underscoreSpan);
-    }
+    // Remove it from possibilities set
+    wordList.delete(answerWord);
 
-    lettersArr.forEach((letter) => {
+    // This line creates a new array that's the same size as the length of the answerWord,
+    // then fills it with underscores to represent unguessed letters.
+    updateCurrentGuess(new Array(answerWord.length).fill('_'));
+
+    // Add event listener to each letter & remove any correct/incorrect classes from before
+    lettersDivArr.forEach((letter) => {
       letter.addEventListener('click', handleLetterClick);
+      letter.classList.remove('correct');
+      letter.classList.remove('incorrect');
     });
 
-    // Set input maxLength
-    // inputEl.maxLength = answerWord.length;
-    // Add eventListener
+    // Remove status class on wordContainer
+    censoredWordContainer.classList.remove('correct');
+    censoredWordContainer.classList.remove('incorrect');
+
+    // clear status box
+    statusBox.innerText = '';
   }
 
   /**
-   * Check if value entered matches hidden word selected (or whatever the fuck)
+   * Function to update the remaining lives left both in memory and on the DOM.
    *
-   * Check if answerWord & key have any letters in common. If so, which?
+   * This is mainly to ensure that our internal currLives and the DOM representation stay in sync
+   */
+  function updateLives(newVal) {
+    currLives = newVal;
+
+    livesDisplay.innerText = currLives;
+  }
+
+  /**
+   * Function to update the currentGuess both in memory and on the DOM.
+   *
+   * This is mainly to ensure that our internal currGuess and the DOM representation stay in sync
+   */
+  function updateCurrentGuess(newCurrentGuess) {
+    currentGuess = newCurrentGuess;
+    censoredWordContainer.innerText = currentGuess.join('');
+  }
+
+  /**
+   * When the game is lost we will display "YOU LOSE!", set all
+   * keys as incorrect, & show what the word was (in red)
+   */
+  function loseGame() {
+    statusBox.innerText = 'YOU LOSE!';
+    statusBox.classList.add('incorrect');
+
+    lettersDivArr.forEach((letter) => {
+      letter.classList.add('incorrect');
+      letter.removeEventListener('click', handleLetterClick);
+    });
+
+    // Show the correct answer but mark it red
+    updateCurrentGuess(answerWord.split(''));
+    censoredWordContainer.classList.add('incorrect');
+  }
+
+  /**
+   * When the game is won we will display "YOU WON!", set
+   * all keys as correct, & color the word green
+   */
+  function winGame() {
+    statusBox.innerText = 'YOU WIN!';
+    statusBox.classList.add('correct');
+
+    lettersDivArr.forEach((letter) => {
+      letter.classList.add('correct');
+      letter.removeEventListener('click', handleLetterClick);
+    });
+
+    // Mark the word green
+    censoredWordContainer.classList.add('correct');
+  }
+
+  /**
+   * This function handles a keypress and checks whether
+   * that pressed key matches any letters in the answerWord.
+   *
+   * If it does,
    */
   function checkValidity(target, key) {
-    /**
-     * Unreadable af but JS flex below
-     * This is the first time I've ever used the comma operator :o
-     *
-     * const indexesArr = answerChars.reduce(
-     *  (acc, curr, i) => (curr === key ? (acc.push(i), acc) : acc),
-     *  [],
-     * );
-     *
-     */
-
+    // If it was an incorrect guess, mark the key as such and return early.
     if (!answerWord.includes(key)) {
       target.classList.add('incorrect');
+      // Decrement lives and handle lose condition
+      updateLives(currLives - 1);
+      if (currLives === 0) loseGame();
       return;
     }
 
-    // We have two strings, answerWord and key.
-    // We need to loop for each instance of key in answerWord
-    // To make this easier let's turn answerWord into an array
-    const answerChars = answerWord.split('');
-    while (answerChars.includes(key)) {
-      target.classList.add('correct');
+    target.classList.add('correct');
 
-      // Find index of letter and replace dash in array with letter
-      const index = answerChars.indexOf(key);
-      answerChars.splice(index, 1);
-      incompArr.splice(index, 1, key);
-      censoredWordContainer.children[index].innerText = incompArr[index];
-    }
+    // Check validity:
+    // We have two strings, answerWord and key.
+    // There might be multiple instances of key in answerWord,
+    // (think key = "i" and answerWord is "twilight"), so we
+    // need to replace the underscores in currentGuess for
+    // each of those instances.
+
+    // Split answerWord into an array
+    const answerChars = answerWord.split('');
+    // Loop through that array and store the index where
+    // the currently guessed letter matches one in the answerWord.
+    answerChars.forEach((letter, i) => {
+      if (letter === key) currentGuess[i] = letter;
+    });
+
+    // Now that we've updated those indicies in currentGuess, we can write to the DOM
+    censoredWordContainer.innerText = currentGuess.join('');
+
+    // Check for win condition by checking if there are any underscores left in currentGuess
+    if (!currentGuess.find((letter) => letter === '_')) winGame();
   }
 
   function handleLetterClick(e) {
@@ -105,6 +194,6 @@ const wordList = [
     checkValidity(target, key);
   }
 
-  // inputEl.addEventListener('keypress', handleClick);
   startGame();
+  restartBtn.addEventListener('click', startGame);
 })();
